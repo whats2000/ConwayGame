f7d9f2d2fc62bfc8c11bbc62f05307afddf8129a
5241513 <96127309+5241513@users.noreply.github.com>
2023-10-19 20:46:59 +0800

    Add UI backgorund
    using panel container, add style in theme overrides, then add margin

--- ConwayGame/UI/StartMenu.cs
+++ ConwayGame/UI/StartMenu.cs
@@ -2,30 +2,30 @@ using Godot;
 
 public partial class StartMenu : Control
 {
-    public static bool isStartMenuOpen = true;
+	public static bool isStartMenuOpen = true;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        Visible = true;
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Visible = true;
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        Scale = Vector2.One / GetParent<Camera2D>().Zoom;
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Scale = Vector2.One / GetParent<Camera2D>().Zoom;
+	}
 
-    private void OnStartButtonPressed()
-    {
-        Visible = false;
-        isStartMenuOpen = false;
-    }
+	private void OnStartButtonPressed()
+	{
+		Visible = false;
+		isStartMenuOpen = false;
+	}
 
 
-    private void OnQuitButtonPressed()
-    {
-        GetTree().Quit();
-    }
+	private void OnQuitButtonPressed()
+	{
+		GetTree().Quit();
+	}
 
 }
